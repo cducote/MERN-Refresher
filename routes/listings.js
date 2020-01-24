ec07d59894012f7c5b76aebc301b5efef9cd2c3e
Chris Ducote <chrisducote@Chriss-MacBook-Air.local>
2020-01-24 11:05:00 +0100

    200 Status empty array

---
+++ routes/listings.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+
+const Listing = require('../models/Listing')
+
+router.get('/', (req, res, next) => {
+  Listing.find()
+  .exec()
+    .then(docs => {
+      console.log(docs);
+      //   if (docs.length >= 0) {
+      res.status(200).json(docs);
+      //   } else {
+      //       res.status(404).json({
+      //           message: 'No entries found'
+      //       });
+      //   }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const morgan = require('morgan')
+const bodyParser = require("body-parser");
 const logger = require('morgan');
-const cookieParser = require('cookie-parser');
+const listingRoutes = require('./routes/listings')
+// const cookieParser = require('cookie-parser');
 const app = express()
 app.use(cors())
 app.use(express.json());
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
+app.use(morgan('dev'))
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
@@ -25,6 +32,24 @@ mongoose.connect(db, { useNewUrlParser: true }).then(() => {
 
 //Quick view
 app.get('/', (req, res) => res.send("BETTY BLOCKS SECRET PROJECT"))
+
+//Routes
+app.use('/listings', listingRoutes)
+
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+  });
+
+  app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+      error: {
+        message: error.message
+      }
+    });
+  });
 
 //Port set
 const port = process.env.PORT || 5000
